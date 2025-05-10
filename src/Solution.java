78a06c32607fd180e9e1902e6c35fc97a27696cc
harshit-sharma-hs <harshitsharma4848@gmail.com>
2025-05-10 13:37:54 +0530

    new project

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+//    static
+//    {
+//        System.out.println("Hello");
+//        System.exit(0);
+//    }
+    public static void main(String[] args) {
+        check();
+        System.out.println("hhdbdbhc");
+    }
+
+    static void check()
+    {
+        System.out.println("check method");
+    }
+
+
+}
